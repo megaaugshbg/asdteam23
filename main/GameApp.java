ddb427d8ad161acad474cc45e9272a775b23dc30
Yonata018 <pandemy26@gmail.com>
2025-12-19 17:38:26 +0800

    FIX GOLLL

--- main/GameApp.java
+++ main/GameApp.java
@@ -5,7 +5,6 @@ import javax.swing.SwingUtilities;
 public class GameApp {
     public static void main(String[] args) {
         // --- 1. JALANKAN MUSIK DI AWAL ---
-        // Ganti "sound4.wav" dengan nama file lagu utama Anda
         SoundManager.playBackgroundMusic("sound4.wav");
 
         // --- 2. JALANKAN UI ---
